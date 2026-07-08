f19659121266585b4dfb4113571d9545aed3a398
Tristan <tristancarter2772@gmail.com>
2026-07-08 13:24:02 -0500

    actual working starter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  compatibilityDate: '2026-07-08',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  compatibilityDate: '2025-07-08'
-})
+  modules: ["@nuxt/eslint", "@nuxt/hints", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      include: [
+        "@vue/devtools-core",
+        "@vue/devtools-kit",
+        // "betterauth/vue",
+      ],
+    },
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
